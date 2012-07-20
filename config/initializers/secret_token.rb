638d082b844950e5de84c2afa2bceca261386e7a
Enrique Macip <emacipbelmonte@gmail.com>
2012-07-20 12:17:55 +0200

    Let there be taggable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taggable::Application.config.secret_token = '94843f3ec0aa2ffc8912d1a7dab5fe4ed1505f413c717ee843d5884807149209fb3359dfe66d93bec9ec92cdb30b9ca5d67d62f103614b73bed4c13e8a44de6d'
